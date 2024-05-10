2eb3318a73ef452294f1a13c6421ebdfcdadeb8b
delbertina <6349928+delbertina@users.noreply.github.com>
2024-05-09 23:38:58 -0500

    Cleanup unused files from default app

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,64 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+
+let releaseDate = ref(9)
+let scoreMoney = ref(1000)
+let scoreCharacters = ref(0)
+let scoreLines = ref(0)
+let scoreFunctions = ref(0)
+let scoreClasses = ref(0)
+let scorePackages = ref(0)
+let scoreUnits = ref(0)
+let scoreProducts = ref(0)
+let scoreCred = ref(0)
+const cooldownAddChar = 5000
+let isAddCharDisabled = ref(false)
+
+const changeRelease = () => {
+  //Change release date to a number from 10-110.
+  releaseDate.value = Math.ceil(Math.random() * 100) + 10
+}
+const addCharacters = () => {
+  //Add characters to total from 2-15
+  scoreCharacters.value += Math.ceil(Math.random() * 15) + 2
+}
+const clickButtonType = () => {
+  if (isAddCharDisabled.value) {
+    return
+  }
+  //Disable button
+  isAddCharDisabled.value = true
+  //Add characters to score
+  addCharacters()
+  //Timer to enable button
+  setTimeout(() => (isAddCharDisabled.value = false), cooldownAddChar)
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>A Blinking Cursor</h1>
+  <p>Days until release: {{ releaseDate }}</p>
+  <button type="button" class="btn btn-primary" v-on:click="changeRelease">Refresh</button>
+  <hr />
+  <h3>Preview:</h3>
+  <p>
+    Money: {{ scoreMoney }}<br />
+    Characters: {{ scoreCharacters }}<br />
+    Lines: {{ scoreLines }}<br />
+    Functions: {{ scoreFunctions }}<br />
+    Classes: {{ scoreClasses }}<br />
+    Packages: {{ scorePackages }}<br />
+    Units: {{ scoreUnits }}<br />
+    Products: {{ scoreProducts }}<br />
+    Cred: {{ scoreCred }}
+  </p>
+  <button
+    type="button"
+    class="btn btn-primary"
+    id="buttonTypeCharacters"
+    :disabled="isAddCharDisabled"
+    v-on:click="clickButtonType"
+  >
+    Type
+  </button>
 </template>
