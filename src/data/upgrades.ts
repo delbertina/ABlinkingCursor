d71a84197268750bd4ef3c8f53b2a4ca495caf4a
delbertina <6349928+delbertina@users.noreply.github.com>
2024-05-19 23:10:09 -0500

    Add placeholder upgrades for the full progression of score types

--- src/data/upgrades.ts
+++ src/data/upgrades.ts
@@ -7,5 +7,33 @@ export const Upgrades: Array<Upgrade> = [
     price: 100,
     priceScoreId: 3,
     description: "We've got enough characters, maybe we need a way to better organize them?"
+  },
+  {
+    id: 2,
+    name: 'Curly Brackets',
+    price: 100,
+    priceScoreId: 4,
+    description: "We've got enough lines, maybe we can do something with a bunch of them?"
+  },
+  {
+    id: 3,
+    name: 'An Object',
+    price: 100,
+    priceScoreId: 5,
+    description: "We've got enough functions, they need something to interact with and be about!"
+  },
+  {
+    id: 4,
+    name: 'Namespace',
+    price: 100,
+    priceScoreId: 6,
+    description: "We've got enough classes, these could be useful for something greater!"
+  },
+  {
+    id: 5,
+    name: 'Build Script',
+    price: 100,
+    priceScoreId: 7,
+    description: "We've got enough packages, maybe we can make something useful for a user!"
   }
 ]
